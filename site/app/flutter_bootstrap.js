1c4425e38eeca97a3744e165603e9fb4fb00a770
4rc4 <arcacicek2@gmail.com>
2026-05-12 00:52:25 +0200

    Deploy Catudy web and APK release

--- site/app/flutter_bootstrap.js
+++ site/app/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"6c0baaebf70e0148f485f27d5616b3d3382da7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3149165839" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2868033234" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
